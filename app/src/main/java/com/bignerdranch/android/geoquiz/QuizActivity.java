dc30f0774097290409c30a7f3a18d3d9c33c1f12
shinabarger <davidshinabarger@gmail.com>
2017-04-20 13:49:03 -0400

    add ids to button, add member variables, retrive ids and assign to member variables

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,13 +1,22 @@
 package com.bignerdranch.android.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    //adds two member variables
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //cast view to button, retrieve inflated objects and assign to member variables
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
     }
 }
